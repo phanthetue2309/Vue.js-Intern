48c2b7d6755702d804d26f20f01e463c71f5aa23
Phan Thế Tuệ <68698678+ptt2309dln0105@users.noreply.github.com>
2021-06-20 21:23:08 +0700

    Update : Using include in string vue.js

--- Learning VueX/learn_vuex/src/App.vue
+++ Learning VueX/learn_vuex/src/App.vue
@@ -2,9 +2,57 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <div>
+      <button
+        v-show="string.includes(substring)"
+        class="mt-3 ml-3 dbuton rbuton text-sm"
+      >
+        LOGIN
+      </button>
+    </div>
+
+    <router-view />
   </div>
-  <router-view />
 </template>
+
+<script>
+export default {
+  name: "Parent",
+  data() {
+    return {
+      string: "login albums:read",
+      substring: "login",
+    };
+  },
+  beforeCreate() {
+    console.log("Parent beforeCreate()");
+  },
+  created() {
+    console.log("Parent created()");
+    if (this.string.includes(this.substring)) {
+      console.log("Contains");
+    }
+  },
+  beforeMount() {
+    console.log("Parent beforeMount()");
+  },
+  mounted() {
+    console.log("Parent mounted()");
+  },
+  beforeUpdate() {
+    console.log("Parent beforeUpdate()");
+  },
+  updated() {
+    console.log("Parent updated()");
+  },
+  beforeUnmount() {
+    console.log("Parent beforeUnmount()");
+  },
+  unmounted() {
+    console.log("Parent unmounted()");
+  },
+};
+</script>
 
 <style>
 #app {
